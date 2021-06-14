00a55bec8ce64cdbec605f73e99e4da7a5748340
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2021-06-14 06:41:00 +0100

    xd

--- src/main/java/codes/biscuit/skyblockaddons/utils/item/ItemUtils.java
+++ src/main/java/codes/biscuit/skyblockaddons/utils/item/ItemUtils.java
@@ -1,6 +1,6 @@
-package codes.biscuit.skyblockaddons.utils.item;
+package codes.sovacos.skyblockaddons.utils.item;
 
-import codes.biscuit.skyblockaddons.constants.game.Rarity;
+import codes.sovacos.skyblockaddons.constants.game.Raridade;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
@@ -19,7 +19,7 @@ public class ItemUtils {
      * @param item the Skyblock item to check
      * @return the rarity of the item if a valid rarity is found, {@code INVALID} if no rarity is found, {@code null} if item is {@code null}
      */
-    public static Rarity getRarity(ItemStack item) {
+    public static Raridade getRaridade(ItemStack item) {
         if (item == null || !item.hasTagCompound()) {
             return null;
         }
@@ -32,18 +32,18 @@ public class ItemUtils {
 
         NBTTagList lore = display.getTagList("Lore", Constants.NBT.TAG_STRING);
 
-        // Determine the item's rarity
+        // Determina a raridade do item.
         for (int i = 0; i < lore.tagCount(); i++) {
             String currentLine = lore.getStringTagAt(i);
 
-            for (Rarity rarity : EnumSet.allOf(Rarity.class)) {
-                if (currentLine.contains(rarity.getTag())) {
-                    return rarity;
+            for (Raridade raridade : EnumSet.allOf(Raridade.class)) {
+                if (currentLine.contains(raridade.getTag())) {
+                    return raridade;
                 }
             }
         }
 
-        // If the item doesn't have a valid rarity, return INVALID
+        // Se o item não tiver uma raridade invalida, INVALID
         return null;
     }
 
